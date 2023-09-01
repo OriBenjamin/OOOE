0d2b4fee1243eb75e1604d2b74abb0fffebb52ba
OriBenjamin <102872346+OriBenjamin@users.noreply.github.com>
2023-09-01 23:42:53 +0300

    add sessions to the api

--- connection.py
+++ connection.py
@@ -1,10 +1,33 @@
-from flask import Flask, request, jsonify
-from typing import Optional
+from datetime import datetime, timedelta
 
+from flask import Flask, request, jsonify, session
+from typing import Optional
+import uuid
 import tables
 from tables import Instruction, Opcode, Operand
 
 app = Flask(__name__)
+app.secret_key = "your_secret_key_here"
+
+sessions = {}
+
+
+def jsonRob(session_tables):
+    jsonROB = []
+    for i in session_tables.ROB:
+        if i[0] is None:
+            jsonROB.append((None, i[1]))
+        else:
+            jsonROB.append((i[0].__dict__, i[1]))
+    return jsonROB
+
+
+def jsonRobCont(session_tables):
+    jsonROB_CONT = []
+    for i in session_tables.ROB_CONT:
+        jsonROB_CONT.append([i[0].name, i[1], getattr(i[2], '__dict__', None), i[3], getattr(i[4], '__dict__', None),
+                             getattr(i[5], '__dict__', None)])
+    return jsonROB_CONT
 
 
 def stringToOperand(string):
@@ -16,53 +39,89 @@ def stringToOperand(string):
         return Operand(int(string), False, False)
 
 
-@app.route('/fetch-instruction', methods=['POST'])
+@app.route('/start-session', methods=['POST'])
+def start_session():
+    session_id = str(uuid.uuid4())
+    sessions[session_id] = {'tables': tables.Tables(), 'expiration': datetime.now()+timedelta(hours=1)}  # create new tables for this session
+    return jsonify(session_id=session_id)
+
+
+@app.route('/end-session', methods=['POST'])
+def end_session():
+    if request.json['session_id'] not in sessions:
+        return jsonify(error='Session not started')
+    del sessions[request.json['session_id']]
+    return jsonify(success=True)
+
+
+@app.route('/fetch', methods=['POST'])
 def fetch_instruction():
+    if request.json['session_id'] not in sessions:
+        return jsonify(error='Session not started')
+    if datetime.now() > sessions[request.json['session_id']]['expiration']:
+        del sessions[request.json['session_id']]
+        return jsonify(error='Session expired')
+    session_tables = sessions[request.json['session_id']]['tables']
     # get the new instruction from the request body
     fetched_instruction = request.json
     instruction = Instruction(Opcode(fetched_instruction['opcode']),
                               (stringToOperand(fetched_instruction['operands'][0]),
                                stringToOperand(fetched_instruction['operands'][1]),
                                stringToOperand(fetched_instruction['operands'][2])))
-    tables.process(instruction)
+    session_tables.process(instruction)
     # Implement your logic to fetch a new instruction
     # Return the current tables along with the fetched instruction (if any)
     # Example response structure:
+    # make ROB json serializable
+
     response = {
         'tables': {
-            'RAT': tables.RAT,
-            'ROB': tables.ROB,
-            'ROB_CONT': tables.ROB_CONT,
-            'cycle': tables.cycle
+            'RAT': session_tables.RAT,
+            'ROB': jsonRob(session_tables),
+            'ROB_CONT': jsonRobCont(session_tables),
         },
     }
     return jsonify(response)
 
 
-@app.route('/run-cycle', methods=['POST'])
+@app.route('/cycle', methods=['POST'])
 def run_cycle():
+    if request.json['session_id'] not in sessions:
+        return jsonify(error='Session not started')
+    if datetime.now() > sessions[request.json['session_id']]['expiration']:
+        del sessions[request.json['session_id']]
+        return jsonify(error='Session expired')
+    session_tables = sessions[request.json['session_id']]['tables']
+
     # Implement your logic to run a cycle
-    tables.cycle()
+    session_tables.cycle()
     # Return the new tables after running the cycle
     response = {
         'tables': {
-            'RAT': tables.RAT,
-            'ROB': tables.ROB,
-            'ROB_CONT': tables.ROB_CONT,
-        }
+            'RAT': session_tables.RAT,
+            'ROB': jsonRob(session_tables),
+            'ROB_CONT': jsonRobCont(session_tables),
+        },
     }
     return jsonify(response)
 
 
-@app.route('/reset', methods=['GET'])
+@app.route('/reset', methods=['POST'])
 def reset():
-    tables.reset()
+    if request.json['session_id'] not in sessions:
+        return jsonify(error='Session not started')
+    if datetime.now() > sessions[request.json['session_id']]['expiration']:
+        del sessions[request.json['session_id']]
+        return jsonify(error='Session expired')
+    session_tables = sessions[request.json['session_id']]['tables']
+
+    session_tables.reset()
     response = {
         'tables': {
-            'RAT': tables.RAT,
-            'ROB': tables.ROB,
-            'ROB_CONT': tables.ROB_CONT,
-        }
+            'RAT': session_tables.RAT,
+            'ROB': jsonRob(session_tables),
+            'ROB_CONT': jsonRobCont(session_tables),
+        },
     }
     return jsonify(response)
 
--- tables.py
+++ tables.py
@@ -54,22 +54,78 @@ class Opcode:
 
 
 # TODO: move to a class
-RAT = [None] * 5
-REGS: list[float] = list(range(5))
-ROB: list[tuple[Optional[Operand], Optional[float]]] = [(None, None)] * 5
-ROB_start = 0
-ROB_CONT = []
-last_physical_register = Operand(0, True, True)
+
+# RAT = [None] * 5
+# REGS: list[float] = list(range(5))
+# ROB: list[tuple[Optional[Operand], Optional[float]]] = [(None, None)] * 5
+# ROB_start = 0
+# ROB_CONT = []
+# last_physical_register = Operand(0, True, True)
 
 
-def reset():
-    global RAT, REGS, ROB, ROB_start, ROB_CONT, last_physical_register
-    RAT = [None] * 5
-    REGS = list(range(5))
-    ROB = [(None, None)] * 5
-    ROB_start = 0
-    ROB_CONT = []
-    last_physical_register = Operand(0, True, True)
+# def reset():
+#     global RAT, REGS, ROB, ROB_start, ROB_CONT, last_physical_register
+#     RAT = [None] * 5
+#     REGS = list(range(5))
+#     ROB = [(None, None)] * 5
+#     ROB_start = 0
+#     ROB_CONT = []
+#     last_physical_register = Operand(0, True, True)
+
+# def process(instruction: Instruction):
+#     global last_physical_register
+#     ROB[last_physical_register.value] = (instruction.operands[0], None)
+#     ROB_CONT.append(
+#         [instruction.opcode, False, instruction.operands[1], False, instruction.operands[2], last_physical_register])
+#     if not instruction.operands[1].is_register:
+#         ROB_CONT[-1][1] = True
+#     else:
+#         if RAT[instruction.operands[1].value] is not None:
+#             ROB_CONT[-1][1] = False
+#             ROB_CONT[-1][2] = Operand(RAT[instruction.operands[1].value], True, True)
+#         else:
+#             ROB_CONT[-1][1] = True
+#             ROB_CONT[-1][2] = Operand(REGS[instruction.operands[1].value], False)
+#     if not instruction.operands[2].is_register:
+#         ROB_CONT[-1][3] = True
+#     else:
+#         if RAT[instruction.operands[2].value] is not None:
+#             ROB_CONT[-1][3] = False
+#             ROB_CONT[-1][4] = Operand(RAT[instruction.operands[2].value], True, True)
+#         else:
+#             ROB_CONT[-1][3] = True
+#             ROB_CONT[-1][4] = Operand(REGS[instruction.operands[2].value], False)
+#     RAT[instruction.operands[0].value] = last_physical_register.value
+#     last_physical_register = Operand(last_physical_register.value + 1, True, True)
+#
+#
+# def process_instructions(instructions):
+#     for instruction in instructions:
+#         process(instruction)
+#
+#
+# def cycle():
+#     global ROB_start
+#     while ROB_start < len(ROB) and ROB[ROB_start][0] is not None and ROB[ROB_start][1] is not None:
+#         REGS[ROB[ROB_start][0].value] = ROB[ROB_start][1]
+#         ROB[ROB_start] = (None, None)
+#         ROB_start += 1
+#     readyValues = []
+#     for inst in ROB_CONT:
+#         if inst[1] and inst[3]:
+#             inst[0].cycles -= 1
+#             if inst[0].cycles == 0:
+#                 ROB[inst[5].value] = (ROB[inst[5].value][0], inst[0].compute(inst[2].value, inst[4].value))
+#                 readyValues.append((inst[5], ROB[inst[5].value][1]))
+#                 ROB_CONT.remove(inst)
+#     for readyValue in readyValues:
+#         for inst in ROB_CONT:
+#             if inst[2] == readyValue[0]:
+#                 inst[1] = True
+#                 inst[2] = Operand(readyValue[1], False)
+#             if inst[4] == readyValue[0]:
+#                 inst[3] = True
+#                 inst[4] = Operand(readyValue[1], False)
 
 
 class Instruction:
@@ -81,81 +137,91 @@ class Instruction:
         return f"Instruction(opcode={self.opcode}, operands={self.operands})"
 
 
-def process(instruction: Instruction):
-    global last_physical_register
-    ROB[last_physical_register.value] = (instruction.operands[0], None)
-    ROB_CONT.append(
-        [instruction.opcode, False, instruction.operands[1], False, instruction.operands[2], last_physical_register])
-    if not instruction.operands[1].is_register:
-        ROB_CONT[-1][1] = True
-    else:
-        if RAT[instruction.operands[1].value] is not None:
-            ROB_CONT[-1][1] = False
-            ROB_CONT[-1][2] = Operand(RAT[instruction.operands[1].value], True, True)
+class Tables:
+    def __init__(self):
+        self.RAT = [None] * 5
+        self.REGS: list[float] = list(range(5))
+        self.ROB: list[tuple[Optional[Operand], Optional[float]]] = [(None, None)] * 5
+        self.ROB_start = 0
+        self.ROB_CONT = []
+        self.last_physical_register = Operand(0, True, True)
+
+    def reset(self):
+        self.RAT = [None] * 5
+        self.REGS: list[float] = list(range(5))
+        self.ROB: list[tuple[Optional[Operand], Optional[float]]] = [(None, None)] * 5
+        self.ROB_start = 0
+        self.ROB_CONT = []
+        self.last_physical_register = Operand(0, True, True)
+
+    def process(self, instruction: Instruction):
+        self.ROB[self.last_physical_register.value] = (instruction.operands[0], None)
+        self.ROB_CONT.append(
+            [instruction.opcode, False, instruction.operands[1], False, instruction.operands[2],
+             self.last_physical_register])
+        if not instruction.operands[1].is_register:
+            self.ROB_CONT[-1][1] = True
         else:
-            ROB_CONT[-1][1] = True
-            ROB_CONT[-1][2] = Operand(REGS[instruction.operands[1].value], False)
-    if not instruction.operands[2].is_register:
-        ROB_CONT[-1][3] = True
-    else:
-        if RAT[instruction.operands[2].value] is not None:
-            ROB_CONT[-1][3] = False
-            ROB_CONT[-1][4] = Operand(RAT[instruction.operands[2].value], True, True)
+            if self.RAT[instruction.operands[1].value] is not None:
+                self.ROB_CONT[-1][1] = False
+                self.ROB_CONT[-1][2] = Operand(self.RAT[instruction.operands[1].value], True, True)
+            else:
+                self.ROB_CONT[-1][1] = True
+                self.ROB_CONT[-1][2] = Operand(self.REGS[instruction.operands[1].value], False)
+        if not instruction.operands[2].is_register:
+            self.ROB_CONT[-1][3] = True
         else:
-            ROB_CONT[-1][3] = True
-            ROB_CONT[-1][4] = Operand(REGS[instruction.operands[2].value], False)
-    RAT[instruction.operands[0].value] = last_physical_register.value
-    last_physical_register = Operand(last_physical_register.value + 1, True, True)
+            if self.RAT[instruction.operands[2].value] is not None:
+                self.ROB_CONT[-1][3] = False
+                self.ROB_CONT[-1][4] = Operand(self.RAT[instruction.operands[2].value], True, True)
+            else:
+                self.ROB_CONT[-1][3] = True
+                self.ROB_CONT[-1][4] = Operand(self.REGS[instruction.operands[2].value], False)
+        self.RAT[instruction.operands[0].value] = self.last_physical_register.value
+        self.last_physical_register = Operand(self.last_physical_register.value + 1, True, True)
+
+    def cycle(self):
+        while self.ROB_start < len(self.ROB) and self.ROB[self.ROB_start][0] is not None and self.ROB[self.ROB_start][1] is not None:
+            self.REGS[self.ROB[self.ROB_start][0].value] = self.ROB[self.ROB_start][1]
+            self.ROB[self.ROB_start] = (None, None)
+            self.ROB_start += 1
+        readyValues = []
+        for inst in self.ROB_CONT:
+            if inst[1] and inst[3]:
+                inst[0].cycles -= 1
+                if inst[0].cycles == 0:
+                    self.ROB[inst[5].value] = (self.ROB[inst[5].value][0], inst[0].compute(inst[2].value, inst[4].value))
+                    readyValues.append((inst[5], self.ROB[inst[5].value][1]))
+                    self.ROB_CONT.remove(inst)
+        for readyValue in readyValues:
+            for inst in self.ROB_CONT:
+                if inst[2] == readyValue[0]:
+                    inst[1] = True
+                    inst[2] = Operand(readyValue[1], False)
+                if inst[4] == readyValue[0]:
+                    inst[3] = True
+                    inst[4] = Operand(readyValue[1], False)
 
 
-def process_instructions(instructions):
-    for instruction in instructions:
-        process(instruction)
-
-
-def cycle():
-    global ROB_start
-    while ROB_start < len(ROB) and ROB[ROB_start][0] is not None and ROB[ROB_start][1] is not None:
-        REGS[ROB[ROB_start][0].value] = ROB[ROB_start][1]
-        ROB[ROB_start] = (None, None)
-        ROB_start += 1
-    readyValues = []
-    for inst in ROB_CONT:
-        if inst[1] and inst[3]:
-            inst[0].cycles -= 1
-            if inst[0].cycles == 0:
-                ROB[inst[5].value] = (ROB[inst[5].value][0], inst[0].compute(inst[2].value, inst[4].value))
-                readyValues.append((inst[5], ROB[inst[5].value][1]))
-                ROB_CONT.remove(inst)
-    for readyValue in readyValues:
-        for inst in ROB_CONT:
-            if inst[2] == readyValue[0]:
-                inst[1] = True
-                inst[2] = Operand(readyValue[1], False)
-            if inst[4] == readyValue[0]:
-                inst[3] = True
-                inst[4] = Operand(readyValue[1], False)
-
-
-def print_tables():
+def print_tables(tables: Tables):
     rat_table = {
-        'Reg': [f"R{i}" for i in range(len(RAT))],
-        'ROB/RRF': ["RRF" if (reg is None) else "ROB" for reg in RAT],
-        'Phys': [f"P{reg}" if (reg is not None) else "" for reg in RAT],
-        'Val': REGS
+        'Reg': [f"R{i}" for i in range(len(tables.RAT))],
+        'ROB/RRF': ["RRF" if (reg is None) else "ROB" for reg in tables.RAT],
+        'Phys': [f"P{reg}" if (reg is not None) else "" for reg in tables.RAT],
+        'Val': tables.RAT
     }
     print("RAT:")
     print(tabulate(rat_table, headers='keys', tablefmt='psql'))
     rob_table = {
-        '#': [f"P{i}" for i in range(len(ROB))],
-        'Valid': [(reg[0] is not None) for reg in ROB],
-        'Dest': [reg[0] if (reg[0] is not None) else "" for reg in ROB],
-        'Data v': [(reg[1] is not None) for reg in ROB],
-        'Data': [reg[1] for reg in ROB]
+        '#': [f"P{i}" for i in range(len(tables.ROB))],
+        'Valid': [(reg[0] is not None) for reg in tables.ROB],
+        'Dest': [reg[0] if (reg[0] is not None) else "" for reg in tables.ROB],
+        'Data v': [(reg[1] is not None) for reg in tables.ROB],
+        'Data': [reg[1] for reg in tables.ROB]
     }
     print("ROB:")
     print(tabulate(rob_table, headers='keys', tablefmt='psql'))
-    rob_cont_table = ROB_CONT
+    rob_cont_table = tables.ROB_CONT
     print("ROB_CONT:")
     print(tabulate(rob_cont_table, headers=['Op', 'src1_ready', 'src1', 'rc2_ready', 'src2', 'dest'],
                    tablefmt='psql'))
